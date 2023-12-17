d68bd5008c71f74b48bcc53a8718071019c5010e
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-12-17 15:26:35 +0100

    Added Database Schema and Added Admin Inlines

--- backend/cjid_archive/core/admin.py
+++ backend/cjid_archive/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+class ImageAdmin(admin.TabularInline):
+    model=ExtractedImages
+
+class DocumentAdmin(admin.ModelAdmin):
+    inlines = [ImageAdmin]
+    list_display = ['title', 'publication_date', 'date_uploaded', 'document']
 
 # Register your models here.
--- backend/cjid_archive/core/models.py
+++ backend/cjid_archive/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import uuid
+
+PROCESS_STATUS = (
+    ('IN PROGRESS', 'IN PROGRESS'),
+    ('PENDING', 'PENDING'),
+    ('COMPLETE', 'COMPLETE'),
+)
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=200, unique=True, editable=False, primary_key=True)
+    publication_date = models.DateField()
+    date_uploaded = models.DateField(auto_now_add=True)
+    extracted_text = models.TextField()
+    document = models.FileField(upload_to="documents/newspapers")
+    processing_status = models.CharField(max_length=200, choices=PROCESS_STATUS)
+
+
+    def __str__(self):
+        return f"{self.title}"
+    
+class ExtractedImages(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
+    page_number = models.CharField(max_length=300)
+    associated_document = models.ForeignKey(Document, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to="newspapers/extracted_images")
+
+    def __str__(self):
+        return f"{self.associated_document.title}-{self.id}-{self.page_number}"
 
 # Create your models here.
