4ffbefc22f9e7e832ca8ad804503a2967f782c05
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-12-17 13:51:54 +0100

    Updated Paths and Views

--- backend/cjid_archive/core/views.py
+++ backend/cjid_archive/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+
+
+def index(request):
+    context = {
+        'title' : 'Dashboard'
+    }
+    return render(request, 'index.html', context)
+
+def archive(request):
+    context = {
+        'title' : 'Archive'
+    }
+    return render(request, 'archive.html', context)
+
+def about(request):
+    context = {
+        'title' : 'About'
+    }
+    return render(request, 'about.html', context)
+
 # Create your views here.
